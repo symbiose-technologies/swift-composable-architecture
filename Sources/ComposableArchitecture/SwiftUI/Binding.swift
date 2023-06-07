02a7c4b5085048b05c4b100e39f34f6e7d4c3d78
Ryan McKinney <rydamckinney@users.noreply.github.com>
2023-06-06 22:23:55 -0400

    Update Binding.swift

--- Sources/ComposableArchitecture/SwiftUI/Binding.swift
+++ Sources/ComposableArchitecture/SwiftUI/Binding.swift
@@ -178,7 +178,7 @@ public struct BindingAction<Root>: Equatable {
   #endif
   let valueIsEqualTo: (Any) -> Bool
 
-  public init(
+  init(
     keyPath: PartialKeyPath<Root>,
     set: @escaping (inout Root) -> Void,
     value: Any,
@@ -252,7 +252,6 @@ extension BindingAction {
     )
   }
 }
-
 
 extension BindingAction {
   /// Transforms a binding action over some root state to some other type of root state given a
