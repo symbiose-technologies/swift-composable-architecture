d8471db87ea060ae8eb848daf98f0b71a45c9a56
Ryan McKinney <rydamckinney@gmail.com>
2023-11-09 10:03:56 -0500

    ForEachStoreContent

--- Sources/ComposableArchitecture/SwiftUI/ForEachStore+Symbiose.swift
+++ Sources/ComposableArchitecture/SwiftUI/ForEachStore+Symbiose.swift
@@ -13,6 +13,8 @@
 ////////////////////////////////////////////////////////////////////////////////
 import OrderedCollections
 import SwiftUI
+import IdentifiedCollections
+
 
 
 
@@ -204,6 +206,60 @@ public struct ForEachStoreWithIDX<
   }
 }
 
+
+
+
+public struct ForEachStoreContent<
+  EachState, EachAction, Data: Collection, ID: Hashable, Content: View
+>: DynamicViewContent {
+  public let data: Data
+  let content: Content
+  /// Initializes a structure that computes views on demand from a store on a collection of data and
+  /// an identified action.
+  ///
+  /// - Parameters:
+  ///   - store: A store on an identified array of data and an identified action.
+  ///   - content: A function that can generate content given a store of an element.
+  public init<EachContent>(
+    _ store: Store<IdentifiedArray<ID, EachState>, (ID, EachAction)>,
+    @ViewBuilder content: @escaping (Int, Store<EachState, EachAction>) -> EachContent
+  )
+  where
+    Data == IdentifiedArray<ID, EachState>,
+    Content == WithViewStore<
+      IdentifiedArray<ID, EachState>, (ID, EachAction),
+      ForEach<IdentifiedArray<ID, EachState>, ID, EachContent>
+    >
+  {
+      let data = store.state.value
+      self.data = data
+//    self.data = store.state.value
+    self.content = WithViewStore(
+      store,
+      observe: { $0 },
+      removeDuplicates: { areOrderedSetsDuplicates($0.ids, $1.ids) }
+    ) { viewStore in
+      ForEach(viewStore.state, id: viewStore.state.id) { element in
+        var element = element
+        let id = element[keyPath: viewStore.state.id]
+        let idx = data.index(id: id)!
+        content(idx,
+          store.scope(
+            state: {
+              element = $0[id: id] ?? element
+              return element
+            },
+            action: { (id, $0) }
+          )
+        )
+      }
+    }
+  }
+
+  public var body: some View {
+    self.content
+  }
+}
 
 
 
